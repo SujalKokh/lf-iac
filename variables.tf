ba289bb4f692b0db7a66db082b7747fd52c3ff0b
Administrator <sujal.kokh@gmail.com>
2021-07-25 16:18:39 +0545

    feat: added flask app docker image url

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "nginx:latest"
+  default     = "530349173736.dkr.ecr.ap-northeast-1.amazonaws.com/test-ecr:latest"
 }
 
 variable "app_port" {
